3af295a930885ce2364fb73944f7c2c5b751ea48
Azamat <urazimbetovazamat6@gmail.com>
2025-04-09 22:01:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/medicalcenter/WalkThroughScreen3.kt
@@ -0,0 +1,20 @@
+package com.example.medicalcenter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WalkThroughScreen3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_walk_through_screen3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
